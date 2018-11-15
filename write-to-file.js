8d5e00a34e7d3f863c66994f167abbbae9e1e14e
Ryan Gewondjan <ryan.gewondjan@gmail.com>
2018-11-14 17:41:34 -0700

    finished main and write to file

---
+++ write-to-file.js
@@ -0,0 +1,28 @@
+const fs = require('fs');
+
+function writeToFile(repositoryName, fileContents, fileExtension) {
+
+    var outputDirectory = './output';
+    var repositoryDirectory = './output/' + repositoryName;
+
+    //If the output folder does not exist, create it
+    if (!fs.existsSync(outputDirectory)) {
+        fs.mkdirSync(outputDirectory);
+    }
+
+    //If the repository folder does not exist, create it
+    if (!fs.existsSync(repositoryDirectory)) {
+        fs.mkdirSync(repositoryDirectory);
+    }
+
+    //Write the file to the output folder
+    fs.writeFile(repositoryDirectory + '/project-info.' + fileExtension, fileContents, function (err) {
+        if (err) {
+            console.log('ERROR: ' + err);
+            return;
+        }
+        console.log('File Written Successfully');
+    });
+
+
+}
