0e537f26f2d2cd593838e7a1774ae92723fedf27
Ryan Gewondjan <ryan.gewondjan@gmail.com>
2018-11-14 17:11:23 -0700

    stubbed out the main method

---
+++ main.js
@@ -0,0 +1,18 @@
+
+
+
+function generateFiles() {
+
+    //1. Call the prompt function and put the JSON object returned into a variable
+    var jsonFile = ""; //substitue "" with name of prompt function.
+    
+    //2. Convert the JSON object to an MD string and save it to a variable
+    var mdFile = jsonToMd(jsonFile);
+
+    //3. Output the jsonFile to a folder
+
+    //4. Output the mdFile to the same folder
+
+    return;
+
+}
